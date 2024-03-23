c2aeb172897c9d28cd88db8abaacf2e4b91152d9
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-24 01:23:32 +0900

    キャンプ場一覧のページ作成

--- YelpCamp/campgrounds/views.py
+++ YelpCamp/campgrounds/views.py
@@ -1,6 +1,9 @@
-# from django.shortcuts import render
 from django.views import generic
 
+from .models import Campground
 
-class IndexView(generic.TemplateView):
+
+class IndexView(generic.ListView):
+    model = Campground
     template_name = "index.html"
+    context_object_name = "campgrounds"
