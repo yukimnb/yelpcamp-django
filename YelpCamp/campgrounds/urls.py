84409c1f479d23b49b1c7c28ad6833a1bbae1ee5
yukimnb <yuki_manabe.dev@icloud.com>
2024-03-25 10:21:35 +0900

    キャンプ場の詳細ページ作成

--- YelpCamp/campgrounds/urls.py
+++ YelpCamp/campgrounds/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import IndexView
+from .views import DetailView, IndexView
 
 app_name = "campgrounds"
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("<int:id>", DetailView.as_view(), name="show"),
 ]
--- YelpCamp/campgrounds/views.py
+++ YelpCamp/campgrounds/views.py
@@ -7,3 +7,10 @@ class IndexView(generic.ListView):
     model = Campground
     template_name = "index.html"
     context_object_name = "campgrounds"
+
+
+class DetailView(generic.DetailView):
+    model = Campground
+    template_name = "show.html"
+    context_object_name = "campground"
+    pk_url_kwarg = "id"
