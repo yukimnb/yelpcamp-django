1721d8a7a01cd0bd9b2bdd7a0d46f5bff5d84954
yukimnb <yuki_manabe.dev@icloud.com>
2024-04-19 23:32:40 +0900

    ロギング設定

--- YelpCamp/YelpCamp/settings/local.py
+++ YelpCamp/YelpCamp/settings/local.py
@@ -6,8 +6,47 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # ロギング設定
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    # フォーマッタ定義
+    "formatters": {
+        "dev": {
+            "format": " {asctime} {name}:[{levelname}]\n{message}",
+            "style": "{",
+        },
+    },
+    # ハンドラ定義
+    "handlers": {
+        # コンソール出力用
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "dev",
+        },
+    },
+    # ロガー定義
+    "loggers": {
+        # Djangoが出力するログ全般を扱うロガー
+        "django": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+        # 自作アプリが出力するログを扱うロガー
+        "campgrounds": {
+            "handlers": ["console"],
+            "level": "DEBUG",
+        },
+        # 発行されるSQLを出力するロガー
+        # "django.db.backends": {
+        #     "handlers": ["console"],
+        #     "level": "DEBUG",
+        # },
+    },
+}
 
 
+# django-debug-toolbar設定
 def show_toobar(request):
     return True
 
